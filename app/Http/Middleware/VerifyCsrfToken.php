7cdb97143193eaa4f5ee2397c72662772af2f5a9
Luis Valencia <stalinvalencia24@gmail.com>
2022-07-20 18:14:11 -0500

    LV

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array<int, string>
-     */
-    protected $except = [
-        //
-    ];
+
+
+    protected $except = [];
+
+    public function __construct(Application $app, Encrypter $encrypter)
+    {
+        parent::__construct($app, $encrypter);
+
+        $this->except[] = "{$this->app->environment('APP_URL')}/api/*";
+    }
+
+
+
 }
